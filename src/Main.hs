8f54b86f3b0d2b451f7faacdaced8d8c832ff464
Thomas Sutton <me@thomas-sutton.id.au>
2013-10-13 12:20:18 +1100

    Mostly just imports

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,45 @@
 module Main ( main ) where
 
+import           Data.Bits
+import           Data.ByteString.Lazy (ByteString)
+import qualified Data.ByteString.Lazy as B
+import           Data.Csv
+import           Data.Map
+import           Data.Set
+import           Data.Vector (Vector)
+import qualified Data.Vector as V
+import           Data.Word
+
+-- | 32 bits ought to be enough for anyone.
+type Attributes = Word32
+
+-- We'll re-use 'Name' from Data.Csv.
+-- type Name = ByteString
+
+-- | A context represents the complete information from which we can derive
+-- concepts.
+data Context = Context { ctxObj :: Set Name
+                       , ctxAtt :: Map Attributes (Name, Name)
+                       , ctxRel :: Map Name Attributes
+                       }
+
+-- | A concept is composed of a set of objects and a set of properties s.t. all
+-- of the properties hold of all of the objects.
+data Concept = Concept { extent :: Set Name
+                       , intent :: Attributes
+                       }
+
 main :: IO ()
 main = do
-  putStrLn "Hello"
+  input <- B.getContents
+  case decode False input of
+    Left err -> putStrLn err
+    Right csv -> do
+      let hd = V.head csv
+          csv' = V.tail csv
+      print $ V.length hd
+      print $ V.length $ massage csv'
+
+-- | Massage the input data so that it has attributes.
+massage :: Vector (Vector ByteString) -> Vector (ByteString)
+massage = V.map (V.head)
