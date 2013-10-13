894a32785a21a5887c3ea7f6e5d4496edf1f668f
Thomas Sutton <me@thomas-sutton.id.au>
2013-10-13 21:44:21 +1100

    Generate more accurate graph
    - Restrict object labels on nodes.
    - Add edges (though not yet restricted to covering nodes only)

--- src/FCA.hs
+++ src/FCA.hs
@@ -16,7 +16,8 @@ module FCA where
 import           Data.ByteString (ByteString)
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BL
-import           Data.Csv
+import           Data.Csv hiding (Name)
+import           Data.List (intersperse, sortBy)
 import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as M
 import           Data.Monoid
@@ -37,6 +38,9 @@ type AttrId = Int
 
 -- | Similarly for objects.
 type ObjId = Int
+
+-- | Names of objects and attributes can be text.
+type Name = Text
 
 -- | An attribute is an ID together with a set of objects which have the
 -- attribute.
@@ -77,20 +81,22 @@ main = do
 -- of maps which can be used to recover the human-readable names for output.
 --
 -- Note that this function assumes that the input data is rectangular.
-parseContext :: Vector (Vector ByteString) -> (Context, Map ObjId Name, Map AttrId Name)
+parseContext :: Vector (Vector Name) -> (Context, Map ObjId Name, Map AttrId Name)
 parseContext csv = let hd      = V.tail $ V.head csv
                        bd      = V.map V.tail $ V.tail csv
+                       os      = V.map V.head $ V.tail csv
                        no      = V.length bd
                        na      = V.length hd
-                       ctx     = V.generate na (\a -> (a, V.ifoldl (\s i v -> if (B.null $ v V.! a) then s else S.insert i s) S.empty bd))
-                       objmap  = V.ifoldl (\m i v -> M.insert i (v V.! 1) m) M.empty bd
+                       ctx     = V.generate na (\a -> (a, V.ifoldl (\s i v -> if (T.null $ v V.! a) then s else S.insert i s) S.empty bd))
+                       objmap  = V.ifoldl (\m i n -> M.insert i n m) (M.singleton (-9999) "_") os
                        attrmap = V.ifoldl (\m i n -> M.insert i n m) M.empty hd
                    in (ctx, objmap, attrmap)
 
-
 -- | Construct the attribute/extent table of a context.
 buildAETable :: Context -> AETable
-buildAETable ctx = snd $ work ctx $ V.singleton (S.empty, S.empty)
+buildAETable ctx = let g = V.foldl (\s v-> S.union s $ snd v) S.empty ctx
+                       t = snd $ work ctx $ V.singleton (S.empty, g)
+                   in t V.++ V.singleton (S.empty, S.empty)
   where
     work :: Context -> AETable -> (Context, AETable)
     work ctx table = maybe (ctx, table)
@@ -105,18 +111,38 @@ generateGraph :: AETable          -- ^ Context lattice table.
               -> Map ObjId Name   -- ^ Map from object ID to name.
               -> Map AttrId Name  -- ^ Map from attribute ID to name.
               -> Text
-generateGraph table omap amap = let cs = map graphNode $ zip [0..] $ V.toList table
-                                    b = fromText "digraph {\n\trankdir=BT;\n" `mappend`
-                                        mconcat cs `mappend`
-                                        fromText "}\n"
-                                in toLazyText b
-  where graphNode (i, (as, ns)) = let label = fromText "label=\"" `mappend` decimal (i) `mappend` fromText "\""
-                                  in mconcat [ fromText "\tconcept" , decimal i
-                                             , fromText " [ "
-                                             , label
-                                             , fromText " ];\n"
-                                             ]
-        
+generateGraph table omap amap =
+  let list   = zip [0..] $ V.toList table
+      list'  = zip [0..] $ V.toList $ restrictObjectLabels table
+  in toLazyText $ mconcat [ fromText "digraph {\n\trankdir=BT;\n\tnode[shape=point,labelloc=t];\nedge[arrowhead=none];\n\n"
+                          , mconcat $ map graphNode $ list'
+                          , mconcat $ map (graphEdges list) $ list
+                          , fromText "}\n"
+                          ]
+  where
+    -- | Generate a @dot@ fragment for the node.
+    graphNode (i, (as, os)) = let attrns = map (amap M.!) $ S.toList as
+                                  objns = map (omap M.!) $ S.toList os
+                                  label = fromText "xlabel=\"" `mappend`
+                                          (mconcat $ intersperse (fromText " ") $ map fromLazyText $ attrns ++ objns) `mappend`
+                                          fromText "\""
+                              in mconcat [ fromText "\tc" , decimal i
+                                         , fromText " [ "
+                                         , label
+                                         , fromText " ];\n\n"
+                                         ]
+    -- | Generate a @dot@ fragment for the edges of a node.
+    graphEdges list (i, (as, os)) = let t = True
+                                        targets = covering i os list
+                                        e i j = mconcat [ fromText "\tc", decimal i, fromText " -> c", decimal j, fromText ";\n"]
+                                    in mconcat [ fromText "\t# Edges for c"
+                                               , decimal i
+                                               , fromText "\n"
+                                               , mconcat $ map (e i) $ map (fst) targets
+                                               ]
+    -- | Find nodes which cover another.
+    covering i s ss = filter (\t@(j,(a,o)) -> j <= i && s `S.isProperSubsetOf` o) ss
+
 -- | Select the maximal attribute in the context.
 --
 -- Here, maximal means is not a proper subset of any other element.
@@ -125,6 +151,15 @@ chooseMax ctx = fmap (flip vselect ctx) $ V.findIndex (f ctx) ctx
   where
     f ctx a = maybe True (const False) $
               V.findIndex (\b-> (snd a) `S.isProperSubsetOf` (snd b)) ctx
+
+-- | Filter the labels in a table such that nodes are mentioned only in the
+-- smallest extent which contains them.
+restrictObjectLabels table = let es = V.foldl (flip S.insert) S.empty $ V.map (snd) table
+                                 os = S.foldl (S.union) S.empty es
+                                 om = S.foldl (\m o -> M.insert o (smallestWith o es) m)
+                                      M.empty
+                                      os
+                             in V.map (\(as,os) -> (as, filterSetByMap os om)) table
 
 -- | Insert a new attribute and it's extent into the table.
 --
@@ -147,10 +182,27 @@ insertAttr (attr, ext) table =
                       in table V.// [(j, r)]
     addIntersects :: AttrId -> Set ObjId -> AETable -> AETable
     addIntersects a e t = let current = V.map (snd) t
-                              new = V.filter (flip V.notElem current) $ V.map (S.intersection e) current
+                              new = V.filter (\s -> (not $ S.null s) && (V.notElem s current)) $ V.map (S.intersection e) current
                           in (t V.++) $ V.map (\x -> (S.empty, x)) new
 
 -- * Utilities
+
+-- | Find the smallest set containing an element.
+--
+-- This is partial.
+smallestWith :: Ord e => e -> Set (Set e) -> Set e
+smallestWith e ss = let candidates = S.filter (S.member e) ss
+                        smallest = head $ sortBy (\x y -> compare (S.size x) (S.size y)) $ S.toList candidates
+                    in smallest
+
+-- | Filter the elements @e@ of @s@ by whether or not @m@ maps @e@ to @s@.
+--
+-- That is, the map @m@ should take @e@ to @s@ is @e@ is a valid member of @s@.
+filterSetByMap :: Ord i =>
+                  Set i -- ^ The set to filter
+               -> Map i (Set i) -- ^ The map
+               -> Set i
+filterSetByMap s m = S.filter (\e -> (m M.! e) == s) s 
 
 -- | Extract and remove the element with the specified index from a vector.
 vselect :: Int -- ^ The index.
