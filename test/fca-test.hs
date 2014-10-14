4901ac4e39baac9d697fd90dea81c7970c14b9b2
Thomas Sutton <me@thomas-sutton.id.au>
2014-10-15 09:36:22 +1100

    Add test stubs to test format reading

--- test/fca-test.hs
+++ test/fca-test.hs
@@ -2,8 +2,10 @@ module Main where
 
 import qualified Data.Set                   as S
 import qualified Data.Vector                as V
+import           System.IO
+import           Test.Hspec
 
-import Data.FCA
+import           Data.FCA
 
 -- | A piece of test data.
 v :: Context
@@ -14,8 +16,26 @@ v = V.fromList [ (1, S.fromList [2,3])
                , (5, S.fromList [2,3,5])
                ]
 
+-- | Test reading and parsing of input data.
+formatsSpec :: Spec
+formatsSpec =
+    describe "reading input formats" $ do
+        it "should read EAV data correctly" $ do
+            input <- openFile "data/fruit.eav" ReadMode
+            hClose input
+            pendingWith "not implemented"
+
+        it "should read tabular data correctly" $ do
+            input <- openFile "data/fruit.csv" ReadMode
+            hClose input
+            pendingWith "not implemented"
+
+        it "should read EA data correctly" $ do
+            input <- openFile "data/fruit.ea" ReadMode
+            hClose input
+            pendingWith "not implemented"
+
 main :: IO ()
 main = do
-  putStrLn "Test!"
-  print v
+    hspec formatsSpec
 
